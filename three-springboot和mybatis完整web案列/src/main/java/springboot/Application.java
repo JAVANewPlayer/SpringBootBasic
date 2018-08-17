b36a60d9bff2934aeb94ec3e30fc69a6bf1eecf0
panlong <panlong@wacai.com>
2018-08-17 08:53:34 +0800

    springboot和mabatis的整合

---
+++ three-springboot和mybatis完整web案列/src/main/java/springboot/Application.java
@@ -0,0 +1,21 @@
+package springboot;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description:
+ * User: huangchengdu
+ * Date: 2018-08-17
+ * Time: 上午8:45
+ */
+
+@SpringBootApplication
+@MapperScan("springboot.dao")
+public class Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
