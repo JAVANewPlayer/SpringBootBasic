3e000d24084648b5824a61ffc83f842f9ff3bfc7
xibeifeng <huang2009303513@gmail.com>
2018-09-01 21:14:08 +0800

    Update

--- springboot的restful增删查改/src/main/java/com/atguigu/springboot/SpringbootWebRestfulcrudApplication.java
+++ springboot的restful增删查改/src/main/java/com/atguigu/springboot/SpringbootWebRestfulcrudApplication.java
@@ -2,6 +2,9 @@ package com.atguigu.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
 
 @SpringBootApplication
 public class SpringbootWebRestfulcrudApplication {
@@ -9,5 +12,18 @@ public class SpringbootWebRestfulcrudApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(SpringbootWebRestfulcrudApplication.class, args);
+    }
+
+    @Bean
+    public ViewResolver myViewResolver(){
+        return new MyViewResolver();
+    }
+
+    private static class MyViewResolver implements ViewResolver{
+
+        @java.lang.Override
+        public View resolveViewName(java.lang.String s, java.util.Locale locale) throws Exception {
+            return null;
+        }
     }
 }
