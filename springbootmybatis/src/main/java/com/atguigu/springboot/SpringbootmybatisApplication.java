6715b83a41d3e5bde152563442ce701379bc63fc
xibeifeng <huang2009303513@gmail.com>
2018-09-05 20:25:26 +0800

    注解版mapper设置完成

--- springbootmybatis/src/main/java/com/atguigu/springboot/SpringbootmybatisApplication.java
+++ springbootmybatis/src/main/java/com/atguigu/springboot/SpringbootmybatisApplication.java
@@ -1,8 +1,10 @@
 package com.atguigu.springboot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan(value = "com.atguigu.springboot.mapper") //批量扫描所有的mapper文件
 @SpringBootApplication
 public class SpringbootmybatisApplication {
 
